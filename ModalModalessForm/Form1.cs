860246f848644b8300973c3c2b903a85d74e134e
Sujeong Lee <olivia7342@naver.com>
2019-06-24 08:56:08 +0900

    3511이수정 창띄우기

--- ModalModalessForm/Form1.cs
+++ ModalModalessForm/Form1.cs
@@ -15,5 +15,20 @@ namespace ModalModalessForm
         {
             InitializeComponent();
         }
+
+        private void 이프로그램은ModalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
